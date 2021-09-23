0ce137fe27a9fa79eb811b663150b6c67343e2af
ji1dev <jiwontak@soongsil.ac.kr>
2021-09-24 02:15:33 +0900

    Add : 메모리 관련 정보 파싱 및 계산 추가

---
+++ assignment2/myps.c
@@ -0,0 +1,360 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <pwd.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <linux/kdev_t.h>   // device 번호 추출을 위한 매크로 정의되어있는 파일
+
+#define _BSD_SOURCE
+#define MAX_PROC 8192
+#define BUF_SIZE 1024
+
+typedef struct procinfo{
+    pid_t pid;              // 프로세스 ID o
+    uid_t uid;              // UID o
+    char username[16];      // 사용자명o
+    double cpu_usage;       // cpu점유율o
+    double mem_usage;       // 메모리 점유율 o
+    unsigned long vsize;    // virtual memory size o
+    unsigned long rss;      // resident set size
+    char tty[16];           // 터미널 번호 o
+    char state[8];          // 상태 
+    char start_time[8];     // 시작시간
+    char time[8];           // 총 CPU사용시간
+    char command[1024];     // 명령어 간략히
+    char command_f[1024];   // 명령어 전체 (옵션 하나라도 붙는 경우)
+} proc;
+
+pid_t cur_pid;              // 현재 pid
+uid_t cur_uid;              // 현재 uid
+char cur_tty[16];           // 현재 tty
+unsigned long total_mem;    // total physical memory
+unsigned long clk_tck;      // num of clock ticks per second of system
+unsigned long uptime;       // uptime of system in seconds
+
+bool options[] = {false, false, false, false}; // a, u, x, r option flag
+proc plist[MAX_PROC];
+
+void init(); // 프로세스 정보 가져오기 전에 필요하거나 미리 설정 가능한 값을 가져오는 함수
+void make_proclist_entry(); // 프로세스의 정보를 파싱 및 가공하는 함수
+void clear_proclist_entry(); // 프로세스 정보를 초기화하는 함수
+void print_proclist(); // 완성된 프로세스 정보 리스트를 출력하는 함수
+
+int get_tty_nr(pid_t pid); // 프로세스의 컨트롤 터미널을 가져오는 함수
+void get_tty(int tty_nr, char tty[16]); // 터미널 정보를 가져오는 함수
+void get_total_mem(); // 물리 메모리 용량을 가져오는 함수
+void get_uptime(); // 시스템의 uptime을 가져오는 함수
+void get_username(); // username 가져오는 함수
+void calc_cpu_usage(char *stat_path, double *ret); // cpu usage를 계산하는 함수
+void get_msize(char *stat_path, unsigned long *vsz, unsigned long *rss); // vsz, rss값을 가져오는 함수
+void calc_mem_usage(unsigned long rss, double *ret); // memory usage를 계산하는 함수
+void get_state(char *pid_path, char state[8]); // state를 가져오는 함수
+
+
+unsigned long convert_to_kb(unsigned long kib); // kib -> kb 단위 변환 함수
+
+int main(int argc, char *argv[]){
+
+    init();
+    make_proclist_entry();
+    //print_proclist();
+
+    for(int i=1; i<argc; ++i){ // get options from argument
+        for(int j=0; j<strlen(argv[i]); ++j){
+            switch(argv[i][j]){
+                case 'a': // all with tty including other users
+                    options[0] = true;
+                    break;
+                case 'u': // user-oriented format
+                    options[1] = true;
+                    break;
+                case 'x': // process without controlling ttys
+                    options[2] = true;
+                    break;
+                case 'r': // only running processes
+                    options[3] = true;
+                    break;
+            }
+        }
+    }
+    return 0;
+}
+
+void init(){
+    cur_uid = getuid(); // current user id
+    cur_pid = getpid(); // current pid
+    clk_tck = sysconf(_SC_CLK_TCK); // clock ticks
+    get_tty(get_tty_nr(cur_pid), cur_tty); // get current tty with tty_nr
+    get_total_mem(); // total memory
+    get_uptime(); // uptime
+    //printf("cur_tty = %s\n", cur_tty);
+}
+
+void make_proclist_entry(){
+    struct dirent *d;
+    struct stat stat_buf; // 각 파일의 정보 구조체
+    char *dirname = "/proc/";
+    DIR *dp = opendir(dirname); // open proc dir
+    while((d = readdir(dp)) != NULL){ // 디렉토리의 각 엔트리를 확인
+        char pid_path[64];
+        bool isProc = true;
+        int name_len = strlen(d->d_name);
+        pid_t p_pid;
+        for(int i=0; i<name_len; ++i){
+            if(!isdigit(d->d_name[i])){
+                isProc = false;
+                break;
+            }
+        }
+        if(!isProc) continue; // 프로세스 파일이 아니면 다음 엔트리로 넘어감
+        strcpy(pid_path, dirname);
+        strcat(pid_path, d->d_name); // 프로세스 파일의 절대 경로
+        sscanf(d->d_name, "%u", &p_pid); // pid 정수로 추출
+
+        struct stat stat_buf;
+        stat(pid_path, &stat_buf); // <pid>파일의 stat 구조체 가져옴
+        if(!S_ISDIR(stat_buf.st_mode)) continue; // 파일이 directory가 아니면 넘어감
+
+        proc proc_entry; // 프로세스 리스트 엔트리 생성
+        clear_proclist_entry(&proc_entry); // 구조체 멤버 초기화
+        
+        char stat_path[64];
+        strcpy(stat_path, pid_path);
+        strcat(stat_path, "/stat");
+        FILE *fp = fopen(stat_path, "r"); // open stat file
+        stat(stat_path, &stat_buf); // stat파일의 stat 구조체 가져옴
+
+        // pid 저장
+        proc_entry.pid = p_pid;
+
+        // uid 저장
+        proc_entry.uid = stat_buf.st_uid;
+
+        // username 저장
+        get_username(proc_entry.uid, proc_entry.username);
+
+        // cpu_usage 저장
+        calc_cpu_usage(stat_path, &proc_entry.cpu_usage);
+
+        // vsz, rss 저장
+        get_msize(stat_path, &proc_entry.vsize, &proc_entry.rss);
+
+        // mem_usage 저장
+        calc_mem_usage(proc_entry.rss, &proc_entry.mem_usage);
+        //printf("%.1lf\n", proc_entry.mem_usage);
+
+        // tty 저장
+        get_tty(get_tty_nr(proc_entry.pid), proc_entry.tty);
+
+        // state 저장
+        get_state(pid_path, proc_entry.state);
+
+        // start_time 저장
+
+        // time 저장
+
+        // command 저장
+
+        // command_f 저장
+    }
+    closedir(dp);
+}
+
+int get_tty_nr(pid_t pid){
+    // tty_nr: controlling terminal of the process -> /proc/[pid]/stat의 7번째 토큰
+    char path[64], buf[BUF_SIZE];
+    sprintf(path, "%s%d%s", "/proc/", (int)pid, "/stat"); // stat 파일 경로 완성
+    FILE *fp = fopen(path, "r"); // open stat file
+    fgets(buf, BUF_SIZE, fp);
+    char *ptr = strtok(buf, " ");
+    int cnt = 1;
+    while(cnt++ < 7) ptr = strtok(NULL, " "); // 7번째 토큰 추출
+    fclose(fp);
+    return atoi(ptr);
+}
+
+void get_tty(int tty_nr, char tty[16]){
+    // pts: pseudo tty slave (major num == 136)
+    // tty: teletype writer (major num == 4)
+    //      -> (minor num < 64): console terminal
+    //      -> (minor num >= 64): serial port
+    int major_n, minor_n;
+    major_n = MAJOR(tty_nr); // 디바이스 구분 위해 사용하는 주 디바이스 번호
+	minor_n = MINOR(tty_nr); // 동일 디바이스 여러개일때 구분 위해 사용하는 부 디바이스 번호    
+	if(major_n == 4){
+		if(minor_n < 64) sprintf(tty, "%s%d", "tty", minor_n); // 콘솔 터미널 환경 e.g) tty1
+		else sprintf(tty, "%sS%d", "tty", minor_n-64); // serial port(line)통한 통신 e.g)ttyS1
+	}
+	else if(major_n == 136){
+		sprintf(tty, "%s/%d", "pts", minor_n); // 원격 터미널 환경 e.g) pts/1
+	}
+	else{
+        sprintf(tty, "%s", "?");
+    }
+}
+
+void get_total_mem(){
+    FILE *fp = fopen("/proc/meminfo", "r"); // open meminfo file
+    unsigned long tmp;
+    char buf[BUF_SIZE];
+    fgets(buf, BUF_SIZE, fp);
+    char *ptr = buf;
+    while(!isdigit(*ptr)) ptr++; // 값 나올때까지 ptr이동
+    sscanf(ptr, "%lu", &tmp); // 숫자만 추출
+    total_mem = convert_to_kb(tmp); // KiB단위를 kB단위로 변환
+    fclose(fp);
+}
+
+void get_uptime(){
+    FILE *fp = fopen("/proc/uptime", "r"); // open uptime file
+    unsigned long tmp;
+    char buf[BUF_SIZE];
+    fgets(buf, BUF_SIZE, fp);
+    char *ptr = strtok(buf, " "); // 첫 값을 가져옴
+    sscanf(ptr, "%lu", &tmp);
+    uptime = tmp;
+    fclose(fp);
+}
+
+void get_username(uid_t uid, char user[32]){
+    char tmp[32];
+    struct passwd *pw = getpwuid(uid);
+    strcpy(tmp, pw->pw_name); // passwd 파일에서 username 추출
+    int user_len = strlen(tmp);
+    if(user_len >= 8) tmp[7] = '+'; // username이 8자리 이상이면 '+'기호로 ellipsis
+    strncpy(user, tmp, 8); // 8자리까지 복사
+    //printf("%s\n", user);
+}
+
+void calc_cpu_usage(char *stat_path, double *ret){
+    unsigned long utime;            // time spent in user mode in clock ticks
+    unsigned long stime;            // time spent in kernel mode in clock ticks
+    unsigned long start_time;       // time when the process started in clock ticks
+    double elapsed_time;            // total elapsed time since process started in seconds
+    unsigned long long total_time;  // total time spent for process in seconds
+    double usage;
+
+    char buf[BUF_SIZE];
+    FILE *fp = fopen(stat_path, "r"); // open stat file
+    fgets(buf, BUF_SIZE, fp);
+
+    char *ptr = strtok(buf, " ");
+    int cnt = 0;
+    while(cnt++ < 22){ // stat 파일에서 14, 15, 22번째 토큰 추출
+        switch(cnt){
+            case 14:
+                sscanf(ptr, "%lu", &utime);
+                break;
+            case 15:
+                sscanf(ptr, "%lu", &stime);
+                break;
+            case 22:
+                sscanf(ptr, "%lu", &start_time);
+                break;
+        }
+        ptr = strtok(NULL, " ");
+    }
+    total_time = utime+stime;
+    elapsed_time = (double)(uptime-(start_time/clk_tck));
+    usage = ((total_time/clk_tck)/elapsed_time)*100;
+    if(usage<0 || usage>100 || isnan(usage) || isinf(usage)) usage = 0; // 표현할 수 없는 값 예외처리
+    *ret = usage;
+    fclose(fp);
+}
+
+void get_msize(char *stat_path, unsigned long *vsz, unsigned long *rss){
+    char buf[BUF_SIZE];
+    FILE *fp = fopen(stat_path, "r"); // open stat file
+    fgets(buf, BUF_SIZE, fp);
+
+    char *ptr = strtok(buf, " ");
+    int cnt = 0;
+    while(cnt++ < 24){ // stat 파일에서 23, 24번째 토큰 추출
+        switch(cnt){
+            case 23:
+                sscanf(ptr, "%lu", vsz); // KiB
+                break;
+            case 24:
+                sscanf(ptr, "%lu", rss); // kB
+                break;
+        }
+        ptr = strtok(NULL, " ");
+    }
+    *vsz = (*vsz)/1024; // Byte -> KiB
+    *rss = (*rss)*4; // (num of working segment + num of code segment pages)*4, kB
+    fclose(fp);
+}
+
+void calc_mem_usage(unsigned long rss, double *ret){
+    // Calculate RSS value, divided by the size of the real memory in use, 
+    // in the machine in KB, times 100, rounded to the nearest full percentage point.
+    // Further, the rounding to the nearest percentage point. 
+    //  -> 출력시 소수점 둘째 자리에서 반올림 후 첫째 자리까지 출력
+    *ret = (double)rss/total_mem*100;
+}
+
+void get_state(char *pid_path, char state[8]){
+    // Additional state characters
+    // <: high-priority (not nice to other users)
+    // N: low-priority (nice to other users)
+    // L: has pages locked into memory (for real-time and custom IO)
+    // s: a session leader
+    // l: multi-threaded (using CLONE_THREAD, like NPTL pthreads do)
+    // +: in the foreground process group
+    char buf[BUF_SIZE], tmp_path[64];
+    strcpy(tmp_path, pid_path);
+    strcat(tmp_path, "/status");
+
+    // FILE *fp = fopen(path, "r"); // open status file to get VmLck value
+    // fgets(buf, BUF_SIZE, fp);
+
+    /*
+    3: state (state of proc) -> 기본 state 'RSDZTW'
+    6: sid (session ID of the proc) -> 's'
+    8: tpgid (ID of foreground process grp of the controlling term of proc) -> '+'
+    19: nice (the nice value, -20(low) ~ 19(high priority)) -> 'N', '<'
+    20: threads (num of threads in this proc) -> 'l'
+    */
+    //fclose(fp);
+}
+
+void clear_proclist_entry(proc *proc_entry){
+    memset(proc_entry->username, '\0', 16);
+    proc_entry->uid = 0;
+    proc_entry->pid = 0;
+    proc_entry->cpu_usage = 0;
+    proc_entry->mem_usage = 0;
+    proc_entry->vsize = 0;
+    proc_entry->rss = 0;
+    memset(proc_entry->tty, '\0', 16);
+    memset(proc_entry->state, '\0', 8);
+    memset(proc_entry->start_time, '\0', 8);
+    memset(proc_entry->time, '\0', 8);
+    memset(proc_entry->command, '\0', 1024);
+    memset(proc_entry->command_f, '\0', 1024);
+}
+
+void print_proclist(){
+    // 항목 필터링 하는 로직 --> 추후 출력 부분에서 활용 예정
+    // char p_tty[16];
+    // get_tty(get_tty_nr(p_pid), p_tty); // 프로세스의 tty 가져옴
+    // if(!options[0]){ // a 옵션 없는 경우, uid가 본인과 다르면 넘어감
+    //     if(stat_buf.st_uid != cur_uid) continue; 
+    // }
+    // if(!options[2]){ // x 옵션 없는 경우, 컨트롤 터미널 없는 프로세스는 넘어감
+    //     if(!strcmp(p_tty, "?")) continue;
+    // }
+    // if(!options[0] && !options[1] && !options[2]){ // a, u, x 옵션 없는 경우, tty가 본인과 다르면 넘어감
+    //     if(strcmp(p_tty, cur_tty)) continue;
+    // }
+}
+
+unsigned long convert_to_kb(unsigned long kib){
+    return kib*1024/1000; // 1.024를 곱함
+}
